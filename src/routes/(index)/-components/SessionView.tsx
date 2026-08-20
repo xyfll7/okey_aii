593f849519f919968da029791869fac625b9100b
xyfll7 <l.7@qq.com>
2026-08-20 10:05:50 +0800

    refactor: remove unused utility import and simplify ChatContent structure

--- src/routes/(index)/-components/SessionView.tsx
+++ src/routes/(index)/-components/SessionView.tsx
@@ -1,6 +1,5 @@
 import { useChatInit } from "#/components/chat/chatInit";
 import { ChatProvider } from "#/components/chat/chatProvider";
-import { cn } from "#/lib/utils";
 import { ChatList } from "#/routes/(index)/-components/ChatList";
 import { Inputer } from "#/routes/(index)/-components/Inputer";
 
@@ -15,11 +14,11 @@ function ChatContent({ session_id }: { session_id: string }) {
 	const msgs = messages.filter((e) => e.role !== "system");
 	const isBusy = status === "submitted" || status === "streaming";
 	return (
-		<div className={cn("relative h-full", "flex flex-col overflow-hidden")}>
+		<>
 			<ChatList msgs={msgs} isBusy={isBusy} />
 			<div className="px-2 pb-2">
 				<Inputer isBusy={isBusy} />
 			</div>
-		</div>
+		</>
 	);
 }
