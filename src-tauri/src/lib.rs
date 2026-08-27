3608be1b75415c94f62badd9b1dc6cd1e372711c
xyfll7 <l.7@qq.com>
2026-08-27 16:37:33 +0800

    refactor: change log level from Info to Debug for improved debugging in development

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -21,7 +21,7 @@ pub fn run() {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
                     tauri_plugin_log::Builder::default()
-                        .level(log::LevelFilter::Info)
+                        .level(log::LevelFilter::Debug)
                         .filter(utils::log_filter::log_filter)
                         .build(),
                 )?;
