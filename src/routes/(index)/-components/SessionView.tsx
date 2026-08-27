5f1280b1435e2b6a8434c4d0da9208347e301fa0
xyf <l.7@qq.com>
2026-08-27 20:59:02 +0800

    feat: implement ChatList component for chat message display and selection handling

--- src/routes/(index)/-components/SessionView.tsx
+++ src/routes/(index)/-components/SessionView.tsx
@@ -1,15 +1,14 @@
 import { ChatProvider } from "#/components/chat/chatProvider";
-import { cn } from "#/lib/utils";
-import { ChatList } from "#/routes/(index)/-components/ChatList";
-import { Inputer } from "#/routes/(index)/-components/Inputer";
-import { SelectedProvider } from "@/store";
+import { SelectedProvider } from "#/store";
+import { ChatList } from "./ChatList";
+import { Inputer } from "./Inputer";
 import LanguageSelector from "./LanguageSelector";
 
 export function SessionView({ session_id }: { session_id: string }) {
 	return (
 		<SelectedProvider>
 			<ChatProvider session_id={session_id}>
-				<div className={cn("h-full", "flex flex-col overflow-hidden")}>
+				<div className="h-full flex flex-col overflow-hidden">
 					<ChatList session_id={session_id} />
 					<div className="px-2 pb-2">
 						<Inputer />
